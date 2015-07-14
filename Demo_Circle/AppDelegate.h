dcf394c77d69c9cf75d2a878bea718f6ef078939
苏阳 <yang.su3@renren-inc.com>
2015-07-14 10:12:02 +0800

    Initial Commit

---
+++ Demo_Circle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo_Circle
+//
+//  Created by SY on 15/7/14.
+//  Copyright (c) 2015年 com.renren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo_Circle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo_Circle
+//
+//  Created by SY on 15/7/14.
+//  Copyright (c) 2015年 com.renren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
